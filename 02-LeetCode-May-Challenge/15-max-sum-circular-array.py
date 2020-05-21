8bbd7ae50f495e96d9eb12f4ac6ce09851c106c6
arjunsehajpal <arjunsehajpal@gmail.com>
2020-05-21 12:03:18 +0530

    updated with Kadane's algorithm

--- 02-LeetCode-May-Challenge/15-max-sum-circular-array.py
+++ 02-LeetCode-May-Challenge/15-max-sum-circular-array.py
@@ -1,20 +1,25 @@
 import math
 
-class Solution:
-    def maxSubarraySumCircular(self, A) -> int:
-        total_sum = 0
-        max_ending_at = 0
-        min_ending_at = 0
-        max_sum = -math.inf
-        min_sum = math.inf
-        
-        for x in A:
-            total_sum += x
-            max_ending_at = max(max_ending_at + x, x)
-            max_sum = max(max_ending_at, max_sum)
-            min_ending_at = min(min_ending_at + x, x)
-            min_sum = min(min_ending_at, min_sum)
-        
-        if max_sum > 0:
-            return max(max_sum, total_sum - min_sum)
-        return max_sum
+def max_sub_array_sum_circular(A):
+    total_sum = 0
+    max_ending_at = 0
+    min_ending_at = 0
+    max_sum = -math.inf
+    min_sum = math.inf
+    
+    for x in A:
+        total_sum += x
+        max_ending_at = max(max_ending_at + x, x)
+        max_sum = max(max_ending_at, max_sum)
+        min_ending_at = min(min_ending_at + x, x)
+        min_sum = min(min_ending_at, min_sum)
+    
+    if max_sum > 0:
+        return max(max_sum, total_sum - min_sum)
+    return max_sum
+
+# driver code
+if __name__=="__main__":
+    arr = [1,-2,3,-2]
+    ans = max_sub_array_sum_circular(arr)
+    print(ans)
